4235518934e104a94a856272163e4d91a7c645ac
devbysalas <rgkalin@gmail.com>
2019-12-30 22:51:05 -0800

    Add image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,33 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
-import Image from "../components/image"
+import Img from "gatsby-image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
   <Layout>
     <SEO title="Home" />
+    <Img
+      className="headshot"
+      fixed={data.file.childImageSharp.fixed}
+      alt="headshot"
+    />
     <h1>CARLOS ALFARO</h1>
     <p>SOFTWARE DEVELOPER</p>
 
   </Layout>
 )
 
+
+export const query = graphql`
+query {
+  file(relativePath: { eq: "profile-pic.jpg" }) {
+    childImageSharp {
+      fixed(width: 125, height: 125) {
+        ...GatsbyImageSharpFixed
+      }
+    }
+  }
+}
+`
 export default IndexPage
